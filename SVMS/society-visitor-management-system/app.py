55da60e65abc863cfd630e7f6afee8072d1b96d6
Dhruv Mishra <dhruvmh50@gmail.com>
2026-07-18 22:28:03 +0530

    Use environment variable for port configuration

--- SVMS/society-visitor-management-system/app.py
+++ SVMS/society-visitor-management-system/app.py
@@ -2132,5 +2132,7 @@ def page_not_found(error):
 init_db()
 
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
